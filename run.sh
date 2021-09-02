10c9c1f1921fa59b737ffc68e0ed42b90e7792fb
Ubuntu <victoriamanicom@gmail.com>
2021-09-02 10:35:00 +0000

    DCP-41 #comment Added database set up to config

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+sudo apt install mysql-client-core-5.7 -y
+cd database
+mysql -h dvk-database.co7ebuoqxhxx.eu-west-1.rds.amazonaws.com -P 3306 -u admin -p${DATABASE_PASSWORD} < Create.sql
+cd ..
 docker-compose build
 docker images
 docker-compose up -d
+docker-compose logs frontend
+docker-compose logs backend
+docker-compose logs database
