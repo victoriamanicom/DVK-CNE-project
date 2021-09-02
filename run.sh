946a17267e9455bd710634abc05d5e316b99a70a
Ubuntu <victoriamanicom@gmail.com>
2021-09-02 11:25:16 +0000

    DCP-41 #comment Added test

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
-cd backend
-export DATABASE_URI=${DATABASE_URI}
-export SECRET_KEY=${SECRET_KEY}
-cd ..
-echo ${DATABASE_URI}
 docker-compose build
-docker images
+python3 -m pytest
 docker-compose up -d
 docker-compose logs frontend
 docker-compose logs backend
