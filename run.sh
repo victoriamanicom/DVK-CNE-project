f65bf68c382f443ba1141bcca45096474cd50f4b
Ubuntu <victoriamanicom@gmail.com>
2021-09-02 14:16:00 +0000

    DCP-18 DCP-16 #comment Installed pytest modules in both dirs, added to shell script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-echo "Webhook successful"
 docker-compose build
 cd frontend
+python3 -m pytest --cov application
+cd ..
+cd backend
 python3 -m pytest --cov application
 cd ..
 docker-compose up -d
