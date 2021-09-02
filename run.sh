fdc6503e92ca7122764955b09f2794585b59dd1a
Ubuntu <victoriamanicom@gmail.com>
2021-09-02 15:40:33 +0000

    DCP-44 #comment Changed output of backend test coverage report

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd frontend
 python3 -m pytest --cov application --cov-report html
 cd ..
 cd backend
-python3 -m pytest --cov application > test-coverage.txt
+python3 -m pytest --cov application --cov-report html
 cd ..
 docker-compose up -d
 docker-compose logs frontend
